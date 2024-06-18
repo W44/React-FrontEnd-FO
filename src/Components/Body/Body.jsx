dd9c72100890238145a0b3fd79c200abe9fc32f7
Arslaan Asad <73607166+W44@users.noreply.github.com>
2024-06-18 21:47:15 +0500

    styling corrections

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -4,17 +4,19 @@ import ListItems from './ListItems';
 import AddItem from './AddItem';
 import { ItemListContext } from '../Contexts/ItemListContext';
 
-export default function Sidebar()
+export default function Body()
 {
     
     const menuCtx = useContext(SidebarContext);
     const ItemCtx = useContext(ItemListContext);
     console.log("Body",ItemCtx)
     return (
-        <div className='w-3/4 px-8 py-16 bg-stone-400 flex rounded-r-xl rounded-l-xl'>
+        <div className='w-3/4 px-8 py-16 bg-stone-400 rounded-r-xl rounded-l-xl'>
         {menuCtx.Menu.MenuItem ==="new" && <AddItem/>}
         {menuCtx.Menu.MenuItem ==="viewCurrent" && ItemCtx.Items.map((item) => {
-            return ( <ListItems name={item.name} price={item.price} description={item.description}/>);
+            return ( <div className=''> 
+            <ListItems name={item.name} price={item.price} description={item.description}/>
+            </div>);
         })}
         {menuCtx.Menu.MenuItem ==="viewPast" && <ListItems name={'Past'} price={30} description={'testing description'}/>}
         </div>
--- src/Components/Body/ListItems.jsx
+++ src/Components/Body/ListItems.jsx
@@ -1,3 +1,4 @@
+import { ListItemStyle } from "../../Constants";
 
 
 
@@ -6,11 +7,11 @@ export default function ListItems({name,price,description,children})
 
 
     return (
-        <>
+        <div className={ListItemStyle}>
         <h3>Order</h3>
         <h4>Name:{name}</h4>
         <h4>Price:{price}</h4>
         <p>Description:{description}</p>
-        </>
+        </div>
     );
 }
